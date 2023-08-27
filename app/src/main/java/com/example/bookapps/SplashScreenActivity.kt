823784cf9265409127519e76b3dfbdb470177a83
Muhamad Rifa James <muhamad.james@graveltechnology.com>
2023-08-27 16:26:27 +0700

    clear

--- app/src/main/java/com/example/bookapps/SplashScreenActivity.kt
+++ app/src/main/java/com/example/bookapps/SplashScreenActivity.kt
@@ -1,23 +1,27 @@
 package com.example.bookapps
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.bookapps.databinding.ActivitySplashScreenBinding
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_splash_screen)
 
-        binding.bookcover.alpha = 0f
-        binding.bookcover.animate().setDuration(1500).alpha(1f).withEndAction(){
-            val i = Intent(this,MainActivity::class.java)
-            startActivity(i)
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-            finish()
+        val splashThread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(3000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
         }
+        splashThread.start()
     }
 }
